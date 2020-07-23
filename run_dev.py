77ef4cd3edc83d2f89e4c606f14ddfa2b9856d62
MOHAMED IDAGHDOUR <midaghdour@gmail.com>
2020-07-23 23:41:37 +0100

    aa

--- run_dev.py
+++ run_dev.py
@@ -1,5 +1,4 @@
 from dev_answer import app
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
